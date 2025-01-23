dad3e2a2f6960e169775de781e60579acaf45373
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-01-23 12:11:16 -0600

    factorial working

--- LectureAssignments/Lecture02/demo_myfuncs.py
+++ LectureAssignments/Lecture02/demo_myfuncs.py
@@ -4,5 +4,16 @@ def sqrt(x, kmax):
         guess = 0.5 * (guess + (x/guess))
     return guess
 
+def factorial(x):
+    if(x == 0):
+        return 1
+    
+    s = 1
+    for i in range(1,x):
+        s = s * (i+1)
+    return s
+
 z = sqrt(19562, 100)
 print(z)
+fac = factorial(7)
+print(fac)
