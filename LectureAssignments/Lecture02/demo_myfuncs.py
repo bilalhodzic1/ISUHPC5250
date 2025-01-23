548983c376a841ae3156d18ea6cb0d3b6b298340
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-01-23 12:33:42 -0600

    Exponential done

--- LectureAssignments/Lecture02/demo_myfuncs.py
+++ LectureAssignments/Lecture02/demo_myfuncs.py
@@ -13,7 +13,21 @@ def factorial(x):
         s = s * (i+1)
     return s
 
+def exp(x):
+    e = 2.7182818284590451
+    x_0 = int(round(x))
+    z = x - x_0
+    e_to_z = 0
+    for i in range(10):
+        val_to_add = ((z**i)/(factorial(i)))
+        print(f"Iter {i} " + str(val_to_add))
+        e_to_z = e_to_z + val_to_add
+    print("e_to_z" + str(e_to_z))
+    return (e**x_0) * (e_to_z)
+
 z = sqrt(19562, 100)
-print(z)
-fac = factorial(7)
-print(fac)
+#print(z)
+fac = factorial(10)
+#print(fac)
+expon = exp(5.782)
+print(expon)
