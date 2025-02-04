e1b761ba54dbf30a10e746e98d349b7a7c27f17e
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-02-04 12:58:22 -0600

    Moved into function. Added adjusted matrix by appending b

--- LectureAssignments/Lecture05/demo_polynomial_interpolation.py
+++ LectureAssignments/Lecture05/demo_polynomial_interpolation.py
@@ -4,20 +4,31 @@ Lecture 5 assignment. Gaussian method
 
 import numpy as np
 
-# def GaussianElimination(A, b):
-#    return 0
 
-x = np.array([[1.0, -2.0, 1.0, 0], [2.0, 1.0, -3.0, 5.0], [4.0, -7.0, 1.0, -1.0]])
+def GaussianElimination(A, b):
+    """
+    Function solves a system using the gaussian Gaussian Elimination method
+    """
+    x = np.append(A, b, axis=1)
+    print(x)
+    num_rows = x.shape[0]
+    num_cols = x.shape[1]
+    for i in range(num_cols - 2):
+        for j in range(i + 1, num_rows):
+            numerator = x[j][i]
+            if numerator == 0:
+                continue
+            denom = x[i][i]
+            scalarval = numerator / denom
+            row_needed = np.copy(x[i])
+            row_needed = row_needed * scalarval
+            x[j] = x[j] - row_needed
+            print("-------------------")
+            print(x)
+    print(x)
 
-num_rows = x.shape[0]
-num_cols = x.shape[1]
 
-for i in range(num_cols - 2):
-    for j in range(i + 1, num_rows):
-        numerator = x[j][i]
-        denom = x[i][i]
-        scalarval = numerator / denom
-        row_needed = np.copy(x[i])
-        row_needed = row_needed * scalarval
-        x[j] = x[j] - row_needed
-print(x)
+test_arr = np.array([[2.0, -2.0, 0], [1.0, -1.0, 1], [0, 3.0, -2.0]])
+test_vector = np.array([[-6.0], [1.0], [-5.0]])
+
+GaussianElimination(test_arr, test_vector)
