2b8bcbc82082c72403b12f6e678549b9b687950d
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-02-04 19:50:21 -0600

    Fixed bug resulting in no triangle for the actual problem

--- LectureAssignments/Lecture05/demo_polynomial_interpolation.py
+++ LectureAssignments/Lecture05/demo_polynomial_interpolation.py
@@ -4,6 +4,8 @@ Lecture 5 assignment. Gaussian method
 
 import numpy as np
 
+np.set_printoptions(linewidth=1000)
+
 
 def GaussianElimination(A, b):
     """
@@ -24,7 +26,10 @@ def GaussianElimination(A, b):
             scalarval = numerator / denom
             row_needed = np.copy(x[i])
             row_needed = row_needed * scalarval
-            x[j] = x[j] - row_needed
+            # subtraction resulted in extremely small decimals sometimes instead of 0
+            # added round of result to 10 decimal places to avoid this
+            # occurs due to
+            x[j] = np.round(x[j] - row_needed, 10)
     # determine number of variables needed to complete the problem and set an array of all none
     num_vars = A.shape[1]
     variables = [None] * num_vars
@@ -52,9 +57,35 @@ def GaussianElimination(A, b):
         # Set varaible if it wasnt. This occurs on last variable we set
         if not is_set:
             variables[0] = (variable_row[num_cols - 1] - cur_addition) / variable_row[0]
+    output_string = ""
+    current_exp = num_vars - 1
+    for i in range(num_vars):
+        if current_exp == 0:
+            output_string += f"{variables[i]}"
+        else:
+            output_string += f"{variables[i]}x^{current_exp} + "
+        current_exp -= 1
+    print(output_string)
+    return output_string
 
 
-test_arr = np.array([[1.0, -2.0, 1.0], [2.0, 1.0, -3.0], [4, -7.0, 1.0]])
-test_vector = np.array([[0], [5.0], [-1.0]])
+x_0 = -0.1
+x_1 = -0.02
+x_2 = 0.02
+x_3 = 0.1
+
+test_arr = np.array(
+    [
+        [1.0, x_0, x_0**2, x_0**3],
+        [1.0, x_1, x_1**2, x_1**3],
+        [1.0, x_2, x_2**2, x_2**3],
+        [1.0, x_3, x_3**2, x_3**3],
+    ]
+)
+# test_arr = np.array(
+#    [[1.0, 2.0, -1.0, 1.0], [-1, 1, 2, -1], [2, -1, 2, 2], [1, 1, -1, 2]]
+# )
+test_vector = np.array([[np.cos(x_0)], [np.cos(x_1)], [np.cos(x_2)], [np.cos(x_3)]])
+# test_vector = np.array([[6.0], [3.0], [14.0], [8.0]])
 
 GaussianElimination(test_arr, test_vector)
