b9778261f3299f6714addf29ee562b7b075c6c5e
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-02-20 12:25:05 -0600

    simple vector functions created

--- LectureAssignments/Lecture10/quad.h
+++ LectureAssignments/Lecture10/quad.h
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct vector {
+  float x;
+  float y;
+  float z;
+} vector_t;
+
 typedef struct point {
   float x;
   float y;
@@ -21,3 +27,5 @@ void quad_area(quadrilateral_t *quad);
 void quad_perimeter(quadrilateral_t *quad);
 void compute_angles(quadrilateral_t *quad);
 void print_details(quadrilateral_t quad);
+vector_t simple_cross_product(vector_t v1, vector_t v2);
+vector_t make_xy_vector(point_t p1, point_t p2);
--- LectureAssignments/Lecture10/quad_area.c
+++ LectureAssignments/Lecture10/quad_area.c
@@ -1,3 +1,18 @@
 #include "quad.h"
 
 void quad_area(quadrilateral_t *quad) {}
+
+vector_t simple_cross_product(vector_t v1, vector_t v2) {
+  vector_t result;
+  result.x = v1.y * v2.z - v2.y * v1.z;
+  result.y = v2.x * v1.z - v1.x * v2.z;
+  result.z = v1.x * v2.y - v2.x - v1.y;
+  return result;
+}
+vector_t make_xy_vector(point_t p1, point_t p2) {
+  vector_t result;
+  result.x = p2.x - p1.x;
+  result.y = p2.y - p1.y;
+  result.z = 0;
+  return result;
+}
