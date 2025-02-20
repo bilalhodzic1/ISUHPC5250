af9997524bdc69f253bc0a2624e6df4923d5d541
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-02-20 12:29:15 -0600

    Wrote area function with double triangle method

--- LectureAssignments/Lecture10/quad_area.c
+++ LectureAssignments/Lecture10/quad_area.c
@@ -1,6 +1,14 @@
 #include "quad.h"
 
-void quad_area(quadrilateral_t *quad) {}
+void quad_area(quadrilateral_t *quad) {
+  vector_t v1 = make_xy_vector(quad->node1, quad->node2);
+  vector_t v2 = make_xy_vector(quad->node2, quad->node3);
+  vector_t v3 = make_xy_vector(quad->node3, quad->node4);
+  vector_t v4 = make_xy_vector(quad->node4, quad->node1);
+  vector_t v1_cross_v2 = simple_cross_product(v1, v2);
+  vector_t v3_cross_v4 = simple_cross_product(v3, v4);
+  quad->area = .5 * fabs(v1_cross_v2.z) + .5 * fabs(v3_cross_v4.z);
+}
 
 vector_t simple_cross_product(vector_t v1, vector_t v2) {
   vector_t result;
