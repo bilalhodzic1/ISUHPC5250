a8c1a91894dfcac4ed1240973b02d9a9c6c6620b
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-02-27 12:52:23 -0600

    Changed to Pop for testing. Works good

--- LectureAssignments/Lecture12/main.c
+++ LectureAssignments/Lecture12/main.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
   purchase_order_t test_po = create_new_po();
   node_t *top;
   push(&top, test_po);
-  purchase_order_t from_the_stack_po = peek(&top);
+  purchase_order_t from_the_stack_po = pop(&top);
   printf("PO NUm: %d\n PO amount: %lf\n PO Descr: %s\n",
          from_the_stack_po.po_number, from_the_stack_po.po_amount,
          from_the_stack_po.po_description);
