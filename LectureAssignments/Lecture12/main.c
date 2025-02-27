df09928fbd789cee5396c8477af52db3736697ef
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-02-27 12:46:34 -0600

    Tested push and peek with create. Working good

--- LectureAssignments/Lecture12/main.c
+++ LectureAssignments/Lecture12/main.c
@@ -3,8 +3,11 @@
 int main(int argc, char *argv[]) {
   display_options();
   purchase_order_t test_po = create_new_po();
-  printf("Purchase order details\n PO Num: %d\n PO amount %lf\n PO description "
-         "%s\n",
-         test_po.po_number, test_po.po_amount, test_po.po_description);
+  node_t *top;
+  push(&top, test_po);
+  purchase_order_t from_the_stack_po = peek(&top);
+  printf("PO NUm: %d\n PO amount: %lf\n PO Descr: %s\n",
+         from_the_stack_po.po_number, from_the_stack_po.po_amount,
+         from_the_stack_po.po_description);
   return 0;
 }
