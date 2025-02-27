0000791e1ec4983e57c503672413874a04c38128
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-02-27 12:40:23 -0600

    Working create new po. Also added flush for fgets

--- LectureAssignments/Lecture12/create_new_po.c
+++ LectureAssignments/Lecture12/create_new_po.c
@@ -2,6 +2,16 @@
 #include <stdio.h>
 
 /**
+ * @brief Flushes new lines from the input to get fgets to work properly.
+ *
+ * @return nothing, function is void
+ */
+void flush_new_line_chars() {
+  while (getchar() != '\n')
+    ;
+}
+
+/**
  * @brief Gets inputs from the user to create a new purchase_order_t.
  *
  * @return A new purchase_order_t created from the user inputs
@@ -9,9 +19,10 @@
 purchase_order_t create_new_po() {
   purchase_order_t new_po;
   printf("Enter a PO number: \n");
-  scanf("%d\n", &new_po.po_number);
+  scanf("%d", &new_po.po_number);
   printf("Enter the total cost of the PO: \n");
-  scanf("%lf\n", &new_po.po_amount);
+  scanf("%lf", &new_po.po_amount);
+  flush_new_line_chars();
   printf("Enter a brief description of the PO:\n");
   fgets(new_po.po_description, sizeof(new_po.po_description), stdin);
   return new_po;
--- LectureAssignments/Lecture12/main.c
+++ LectureAssignments/Lecture12/main.c
@@ -2,5 +2,9 @@
 
 int main(int argc, char *argv[]) {
   display_options();
+  purchase_order_t test_po = create_new_po();
+  printf("Purchase order details\n PO Num: %d\n PO amount %lf\n PO description "
+         "%s\n",
+         test_po.po_number, test_po.po_amount, test_po.po_description);
   return 0;
 }
