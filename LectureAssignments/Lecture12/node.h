9076e441f1f56789239dd0bcc94b96ccb64ddf0b
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-02-27 12:15:44 -0600

    made push function

--- LectureAssignments/Lecture12/node.h
+++ LectureAssignments/Lecture12/node.h
@@ -1,10 +1,9 @@
 #ifndef __NODE_H__
 #define __NODE_H__
 
-typedef struct node node_t;
-struct node {
-  node_t *next;
-};
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 typedef struct purchase_order {
   int po_number;
@@ -12,14 +11,20 @@ typedef struct purchase_order {
   char *po_description;
 } purchase_order_t;
 
+typedef struct node node_t;
+struct node {
+  purchase_order_t po;
+  node_t *next;
+};
+
 void execute_option(int option);
 int query_option();
 void delete_stack(node_t **top);
 void display_options();
 void push(node_t **top, purchase_order_t new_po);
-purchase_order_t pop(node **top);
-purchase_order_t peek(node **top);
-void display_stack(node **top);
-int get_stack_size(node **top);
+purchase_order_t pop(node_t **top);
+purchase_order_t peek(node_t **top);
+void display_stack(node_t **top);
+int get_stack_size(node_t **top);
 
 #endif
---
+++ LectureAssignments/Lecture12/push.c
@@ -0,0 +1,14 @@
+#include "node.h"
+
+void push(node_t **top, purchase_order_t new_po) {
+  if (top == NULL) {
+    *top = (node_t *)malloc(sizeof(node_t));
+    (*top)->next = NULL;
+    (*top)->po = new_po;
+  } else {
+    node_t *new_node = (node_t *)malloc(sizeof(node_t));
+    new_node->po = new_po;
+    new_node->next = (*top);
+    *top = new_node;
+  }
+}
