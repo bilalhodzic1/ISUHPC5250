b49811158b58e9bc7eee666fc54e2005bfa5853e
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-02-27 20:46:40 -0600

    Added doxygen descriptions to the print functions

--- LectureAssignments/Lecture12/display_stack.c
+++ LectureAssignments/Lecture12/display_stack.c
@@ -1,6 +1,12 @@
 #include "node.h"
 #include <stdio.h>
-
+/**
+ * @brief Prints the details associated with one done. then recursively prints
+ * the next
+ *
+ * @param  stack_node the node of the stack you want to print
+ * @return nothing, function is void
+ */
 void print_node(node_t *stack_node) {
   printf("|%15p|%15p|%10d|%10lf|%s", stack_node, stack_node->next,
          stack_node->po.po_number, stack_node->po.po_amount,
@@ -12,7 +18,12 @@ void print_node(node_t *stack_node) {
   }
   print_node(stack_node->next);
 }
-
+/**
+ * @brief Function deisplays the stack with all the headers and annotations
+ *
+ * @param  top a reference to the top node of the stack
+ * @return nothing, function is void
+ */
 void display_stack(node_t **top) {
   printf("\n");
   printf("|Purchase order stack|\n");
