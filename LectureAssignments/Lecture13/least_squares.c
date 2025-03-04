473f216ea9070f40d46d50c2561a2f29dac9f1df
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-03-04 16:54:16 -0600

    Bug fixed. Works for test case. Great success

--- LectureAssignments/Lecture13/least_squares.c
+++ LectureAssignments/Lecture13/least_squares.c
@@ -3,8 +3,8 @@
 matrix transpose_matrix(matrix *mat) {
   matrix mat_t = new_matrix(mat->cols, mat->rows);
   int i, j;
-  for (i = 0; i < mat_t.rows; i++) {
-    for (j = 0; j < mat_t.cols; j++) {
+  for (i = 1; i <= mat_t.rows; i++) {
+    for (j = 1; j <= mat_t.cols; j++) {
       mget(mat_t, i, j) = mgetp(mat, j, i);
     }
   }
@@ -19,11 +19,10 @@ vector least_squares(const vector *x, const vector *f, const int k) {
       mget(A, i, j + 1) = pow(vgetp(x, i), j);
     }
   }
-  print_matrix(&A);
   if (x->size == k) {
     return solve(&A, f);
   } else {
-    matrix A_T = new_matrix(A.cols, A.rows);
+    matrix A_T = transpose_matrix(&A);
     matrix to_solve_matrix = matrix_mult(&A_T, &A);
     vector right_result = matrix_vector_mult(&A_T, f);
     return solve(&to_solve_matrix, &right_result);
