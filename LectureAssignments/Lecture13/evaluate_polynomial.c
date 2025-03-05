280a3fdc96c5fb8c7e2b5bdd16b38b148d293a6e
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-03-04 18:23:37 -0600

    Removed unneeded print statement

--- LectureAssignments/Lecture13/evaluate_polynomial.c
+++ LectureAssignments/Lecture13/evaluate_polynomial.c
@@ -15,7 +15,6 @@ vector evaluate_polynomial(vector *x_points, vector *coeffecients, int degree,
   vector y_points = new_vector(num_points);
   int i, j;
   for (i = 1; i <= num_points; i++) {
-    printf("%d\n", coeffecients->size);
     for (j = 0; j <= degree; j++) {
       vget(y_points, i) =
           pow(vgetp(x_points, i), j) * vgetp(coeffecients, j + 1);
