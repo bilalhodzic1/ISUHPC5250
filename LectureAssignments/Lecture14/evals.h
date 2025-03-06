8e71b08610202d38d631820e623dfca0384f503f
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-03-06 12:15:38 -0600

    Defined all needed functions

---
+++ LectureAssignments/Lecture14/evals.h
@@ -0,0 +1,9 @@
+#ifndef __EVALS_H__
+#define __EVALS_H__
+#include "matrix.h"
+
+double power_iteration(matrix A);
+double inverse_power_iteration(matrix A, double mu);
+double rayleigh_quotient_iteration(matrix A, vector v_0);
+
+#endif
---
+++ LectureAssignments/Lecture14/main.c
@@ -0,0 +1,3 @@
+#include "matrix.h"
+
+int main(int argc, char *argv[]) { return 0; }
