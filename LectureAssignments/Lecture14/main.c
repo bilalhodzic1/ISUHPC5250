f7b726547619c4159004949a69b6943b24d85a32
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-03-07 21:10:21 -0600

    Fixed power iteration inverse

--- LectureAssignments/Lecture14/main.c
+++ LectureAssignments/Lecture14/main.c
@@ -18,7 +18,10 @@ int main(int argc, char *argv[]) {
   double inverse_power = inverse_power_iteration(&A, 4.2);
   printf("%lf\n", inverse_power);
   vector v_0 = new_vector(A.cols);
-  fill_random_vector(&v_0);
+  int i;
+  for (i = 0; i < A.rows; i++) {
+    vget(v_0, i + 1) = mget(A, i + 1, i + 1);
+  }
   double rayleigh_power = rayleigh_quotient_iteration(&A, &v_0);
   printf("%lf\n", rayleigh_power);
   return 0;
