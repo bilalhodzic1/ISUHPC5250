f8d9ddab1b0e319918dd12839d556511473c06f9
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-03-07 22:22:32 -0600

    Comment main

--- LectureAssignments/Lecture14/main.c
+++ LectureAssignments/Lecture14/main.c
@@ -2,18 +2,24 @@
 #include "matrix.h"
 
 int main(int argc, char *argv[]) {
+  // Generate matrix A
   matrix A = generate_matrix_for_solve();
+  // Print what we got
   print_matrix(&A);
+  // Conduct power_iteration
   double test_eval = power_iteration(&A);
-  printf("%lf\n", test_eval);
+  printf("Power: %lf\n", test_eval);
+  // Conduct inverse_power_iteration
   double inverse_power = inverse_power_iteration(&A, 4.2);
-  printf("%lf\n", inverse_power);
+  printf("Inverse power: %lf\n", inverse_power);
+  // Make a vector the diagnoal of A
   vector v_0 = new_vector(A.cols);
   int i;
   for (i = 0; i < A.rows; i++) {
     vget(v_0, i + 1) = mget(A, i + 1, i + 1);
   }
+  // Conduct rayleigh_quotient_iteration
   double rayleigh_power = rayleigh_quotient_iteration(&A, &v_0);
-  printf("%lf\n", rayleigh_power);
+  printf("Rayleigh: %lf\n", rayleigh_power);
   return 0;
 }
