d28002f2a52bbe9177cf41e13b0be1a3b5e1c9dd
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-03-25 17:44:54 -0500

    Actually output correctly. Also called the function

--- LectureAssignments/Lecture17/main.c
+++ LectureAssignments/Lecture17/main.c
@@ -4,5 +4,6 @@ int main(int argc, char *argv[]) {
   int num_threads = atoi(argv[1]);
   int vector_size = atoi(argv[2]);
   int degree_k = atoi(argv[3]);
+  make_output_vector(num_threads, vector_size, degree_k);
   return 0;
 }
