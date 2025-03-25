a92b1435383fcfa4c1b46bc7779e2de292579e2f
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-03-25 12:36:56 -0500

    Scaffolded lecture 17

---
+++ LectureAssignments/Lecture17/main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(int argc, char *argv[]) {
+  int num_threads = atoi(argv[1]);
+  int vector_size = atoi(argv[2]);
+  int degree_k = atoi(argv[3]);
+  return 0;
+}
---
+++ LectureAssignments/Lecture17/main.h
@@ -0,0 +1,6 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+#include "matrix.h"
+#include <stdio.h>
+#include <stdlib.h>
+#endif
