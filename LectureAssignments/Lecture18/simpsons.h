b8a26ace8d9eff38c66e4bc6bbd251fde4ad3df4
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-03-27 16:25:37 -0500

    Added openmp declaration

--- LectureAssignments/Lecture18/simpsons.h
+++ LectureAssignments/Lecture18/simpsons.h
@@ -3,6 +3,9 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 double func(const double x);
 double simpson_computation(double val_a, double val_b, int N);
 double local_simpson_computation(double val_a, double val_b, int N);
