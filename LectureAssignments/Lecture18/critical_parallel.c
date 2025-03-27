f7d091561515b0fed4704843ee56cc4c9fa4ebaf
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-03-27 16:05:23 -0500

    Fixed num thread error

--- LectureAssignments/Lecture18/critical_parallel.c
+++ LectureAssignments/Lecture18/critical_parallel.c
@@ -3,7 +3,7 @@ double critical_simpson_computation(double val_a, double val_b, int N,
                                     int num_threads) {
   double T = 0.0;
   const double time1 = omp_get_wtime();
-#pragma omp parallel num_threads(thread_count)
+#pragma omp parallel num_threads(num_threads)
   {
     double T_local = local_simpson_computation(val_a, val_b, N);
 #pragma omp critical
--- LectureAssignments/Lecture18/reduction_parallel.c
+++ LectureAssignments/Lecture18/reduction_parallel.c
@@ -3,7 +3,7 @@ double reduction_simpson_computation(double val_a, double val_b, int N,
                                      int num_threads) {
   double T = 0.0;
   const double time1 = omp_get_wtime();
-#pragma omp parallel num_threads(thread_count) reduction(+ : T)
+#pragma omp parallel num_threads(num_threads) reduction(+ : T)
   T += local_simpson_computation(val_a, val_b, N);
   const double time2 = omp_get_wtime();
   return T;
