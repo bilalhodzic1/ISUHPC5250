4bf6ec71adcb0469cae4523a4ec0153e82d27c1e
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-03-27 22:15:44 -0500

    loigging

--- LectureAssignments/Lecture18/critical_parallel.c
+++ LectureAssignments/Lecture18/critical_parallel.c
@@ -10,6 +10,8 @@ double critical_simpson_computation(double val_a, double val_b, int N,
     T += T_local;
   }
   const double time2 = omp_get_wtime();
+  printf("%lf time 1\n", time1);
+  printf("%lf time 2\n", time2);
   const double clock_time = time2 - time1;
   printf("With %i threads , clock_time = %11.5e (sec)\n", num_threads,
          clock_time);
