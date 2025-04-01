621716ceda2159f3326df0f088ca128e620985e0
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-04-01 12:31:33 -0500

    Made max normalize fine vector fucntion

--- LectureAssignments/Lecture19/max_normalize_vector_fine.c
+++ LectureAssignments/Lecture19/max_normalize_vector_fine.c
@@ -1,3 +1,33 @@
 #include "main.h"
+#include "matrix.h"
 
-void max_normalize_vector_fine(vector v, int num_threads, int N) {}
+void max_normalize_vector_fine(vector v, int num_threads, int N) {
+  for (int i = 1; i <= N; i++) {
+    vget(v, i) = N;
+  }
+
+  double norm = 0.0;
+
+  const double time1 = omp_get_wtime();
+
+#pragma omp parallel num_threads(thread_count)
+  {
+#pragma omp for reduction(max : norm)
+    for (int i = 1; i <= N; i++) {
+      if (vget(v, 1) > norm) {
+        norm = vget(v, i);
+      }
+    }
+#pragma omp for
+    for (int i = 1; i <= N; i++) {
+      vget(v, i) = vget(v, i) / norm;
+    }
+  }
+
+  const double time2 = opm_get_wtime();
+  const double clock_time = time2 - time1;
+
+  printf("Num Threads: %d, Norm: %lf, V[0]: %lf, V[N-1]: %lf, Clock time: "
+         "%11.5e\n",
+         num_threads, norm, vget(v, 1), vget(v, N), clock_time);
+}
