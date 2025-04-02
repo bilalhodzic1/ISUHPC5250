4135887363fcbbe3084b1a2baf85a6771ccafd93
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-04-01 20:26:02 -0500

    Fixed critical zone and added print statement for logging

--- LectureAssignments/Lecture19/max_normalize_vector_coarse.c
+++ LectureAssignments/Lecture19/max_normalize_vector_coarse.c
@@ -5,7 +5,8 @@ void max_normalize_vector_coarse(int num_threads, int N) {
   for (int i = 1; i <= N; i++) {
     vget(v, i) = i;
   }
-
+  const double time1 = omp_get_wtime();
+  double norm = 0.0;
 #pragma omp parallel num_threads(num_threads)
   {
     const int local_rank = omp_get_thread_num();
@@ -14,13 +15,17 @@ void max_normalize_vector_coarse(int num_threads, int N) {
     const int local_i_start = local_rank * N_per_thread + 1;
     const int local_i_end = (local_rank + 1) * N_per_thread;
 
-    double norm = 0.0;
+    double thread_norm = 0.0;
     for (int i = local_i_start; i <= local_i_end; i++) {
+      if (vget(v, i) > thread_norm) {
+        thread_norm = vget(v, i);
+      }
+    }
+
 #pragma omp critical
-      {
-        if (vget(v, i) > norm) {
-          norm = vget(v, i);
-        }
+    {
+      if (thread_norm > norm) {
+        norm = thread_norm;
       }
     }
 #pragma omp barrier
@@ -29,4 +34,11 @@ void max_normalize_vector_coarse(int num_threads, int N) {
       vget(v, i) = vget(v, i) / norm;
     }
   }
+  const double time2 = omp_get_wtime();
+  const double clock_time = time2 - time1;
+
+  printf("Max norm fine : [Num Threads: %d, Norm: %lf, V[0]: %lf, V[N-1]: %lf, "
+         "Clock time: "
+         "%11.5e]\n",
+         num_threads, norm, vget(v, 1), vget(v, N), clock_time);
 }
