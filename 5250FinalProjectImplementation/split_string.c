5e537c2a8166d625f4b8dadf7af7241bc5e812b3
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-05-01 11:20:09 -0500

    changed to strtok to conform to standards

--- 5250FinalProjectImplementation/split_string.c
+++ 5250FinalProjectImplementation/split_string.c
@@ -1,21 +1,26 @@
 #include "csvutils.h"
 /**
-* Splits a string on a delmitter
-*
-* @param str a string to be split
-* @param delimiter a string to be used as a delimiter
-* @param num_tokens an int pointer to be set to num of strings in array
-* @return an array of strings containing the split values
-*/
-char** split_string(char* str, char* delimiter, int* num_tokens){
-    char *token;
-    char *rest = str;
-    char** tokens = malloc(sizeof(char*) * 19);
-    int index = 0;
-    while ((token = strsep(&rest, delimiter)) != NULL) {
-        tokens[index] = token;
-        index++;
-    }
-    *num_tokens = 19;
-    return tokens;
+ * Splits a string on a delmitter
+ *
+ * @param str a string to be split
+ * @param delimiter a string to be used as a delimiter
+ * @param num_tokens an int pointer to be set to num of strings in array
+ * @return an array of strings containing the split values
+ */
+char **split_string(char *str, char *delimiter, int *num_tokens) {
+  char *copy = strdup(str); // Make a modifiable copy of the string
+  if (!copy)
+    return NULL;
+
+  char **tokens = malloc(sizeof(char *) * 19); // Adjust if needed
+  int index = 0;
+
+  char *token = strtok(copy, delimiter);
+  while (token != NULL && index < 19) {
+    tokens[index++] = token;
+    token = strtok(NULL, delimiter);
+  }
+
+  *num_tokens = index;
+  return tokens;
 }
