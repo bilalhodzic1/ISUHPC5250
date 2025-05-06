3f197878d07f683b63c90b9eb7bb23a69bc7841e
Bilal Hodzic <75500195+bilalhodzic1@users.noreply.github.com>
2025-05-06 08:57:48 -0500

    merged hashmaps

--- 5250FinalProjectImplementation/main.c
+++ 5250FinalProjectImplementation/main.c
@@ -70,19 +70,70 @@ int main(int argc, char *argv[]) {
     int local_count = sendcounts[my_rank];
     game_t *local_games = (game_t *)malloc(local_count * sizeof(game_t));
     player_and_agg_t *local_player_agg_array = NULL;
+    int player_count;
     if (my_rank == 0) {
       scatter_game_array(my_rank, sendcounts, process_displacements,
                          mpi_game_obj, NULL, local_count, games);
     } else {
       scatter_game_array(my_rank, sendcounts, process_displacements,
                          mpi_game_obj, local_games, local_count, games);
-      int player_count;
       local_player_agg_array = compute_local_player_agg_array(
           local_games, local_count, &player_count);
     }
+    MPI_Barrier(MPI_COMM_WORLD);
+    int *player_counts = NULL;
     if (my_rank == 0) {
+      player_counts = malloc(comm_sz * sizeof(int));
+    }
+    MPI_Gather(&player_count, 1, MPI_INT, player_counts, 1, MPI_INT, 0,
+               MPI_COMM_WORLD);
+    int *gather_player_displacements = NULL;
+    player_and_agg_t *global_players_array = NULL;
+    int total_players = 0;
+    if (my_rank == 0) {
+      gather_player_displacements = malloc(comm_sz * sizeof(int));
+      gather_player_displacements[0] = 0;
+      for (int i = 1; i < comm_sz; i++) {
+        gather_player_displacements[i] =
+            gather_player_displacements[i - 1] + player_counts[i - 1];
+      }
+      total_players =
+          gather_player_displacements[comm_sz - 1] + player_counts[comm_sz - 1];
 
-    } else {
+      global_players_array = malloc(total_players * sizeof(player_and_agg_t));
+    }
+    MPI_Gatherv(local_player_agg_array, player_count, mpi_player_and_agg_obj,
+                global_players_array, player_counts,
+                gather_player_displacements, mpi_player_and_agg_obj, 0,
+                MPI_COMM_WORLD);
+    if (my_rank == 0) {
+      int actual_player_count = 0;
+      for (int i = 0; i < total_players; i++) {
+        HashItem *complete_player_agg_map = NULL;
+        HashItem *item =
+            find(global_players_array[i].player_id, &complete_player_agg_map);
+        if (item) {
+          stat_agg_t *existing_stat_agg = item->value;
+          stat_agg_t new_stat_agg = global_players_array[i].player_agg_stats;
+          existing_stat_agg->fgm_agg += new_stat_agg.fgm_agg;
+          existing_stat_agg->ftm_agg += new_stat_agg.ftm_agg;
+          existing_stat_agg->oreb_agg += new_stat_agg.oreb_agg;
+          existing_stat_agg->dreb_agg += new_stat_agg.dreb_agg;
+          existing_stat_agg->ast_agg += new_stat_agg.ast_agg;
+          existing_stat_agg->stl_agg += new_stat_agg.stl_agg;
+          existing_stat_agg->blk_agg += new_stat_agg.blk_agg;
+          existing_stat_agg->fga_agg += new_stat_agg.fga_agg;
+          existing_stat_agg->fta_agg += new_stat_agg.fta_agg;
+          existing_stat_agg->to_agg += new_stat_agg.to_agg;
+          existing_stat_agg->pf_agg += new_stat_agg.pf_agg;
+          existing_stat_agg->mins_agg += new_stat_agg.mins_agg;
+        } else {
+          actual_player_count++;
+          insert(global_players_array[i].player_id,
+                 &(global_players_array[i].player_agg_stats),
+                 &complete_player_agg_map);
+        }
+      }
     }
   }
   MPI_Barrier(MPI_COMM_WORLD);
